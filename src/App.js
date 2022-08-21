16ce030806bc6022363554ed55632946212e926a
cgdmohamed <cgd_mohamed@outlook.com>
2022-08-21 10:35:55 +0200

    owl installed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import "boxicons";
 import React from "react";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
